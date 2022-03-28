e95459b9921c4f6411287498921c5883c5c84987
sofiedroid <sofie.teugels@hotmail.com>
2022-03-28 15:47:30 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import subprocess
 import pandas as pd
 import json
-from urllib.request import urlopen
-from io import BytesIO
+
 
 ## 1. fetch json from LDES
-import requests as requests
-from PIL.Image import Image
 
 context = r"C:\Users\teugelso\PycharmProjects\pythonProject\TraintheTrainer_05\src\utils\context.jsonld"
 
 endpoint = f"actor-init-ldes-client --pollingInterval 5000 --mimeType application/ld+json --context " + context + r" --fromTime 2022-03-20T00:00:00.309Z --emitMemberOnce false --disablePolling true https://apidg.gent.be/opendata/adlib2eventstream/v1/dmg/objecten > C:\Users\teugelso\PycharmProjects\pythonProject\TraintheTrainer_05\src\data\dmg_obj.json"
 filepath = r"C:\Users\teugelso\PycharmProjects\pythonProject\TraintheTrainer_05\src\data\dmg_obj.json"
 
-#subprocess.run(endpoint, shell=True, text=True)
+subprocess.run(endpoint, shell=True, text=True)
 
 ## fetch_json(filepath)
 
@@ -27,7 +24,7 @@ def generate_dataframe(filepath):
         return result
 
 df_dmg = pd.DataFrame(generate_dataframe(filepath))
-columns = ["URI", "title", "beschrijving", "objectnummer", "object_name", "association"]
+columns = ["URI", "title", "beschrijving", "objectnummer", "object_name"]
 
 ##define actions
 
@@ -73,19 +70,6 @@ def fetch_objectname(df_dmg, range, _json):
     except Exception:
         pass
 
-def fetch_association(df_dmg, range_, _json):
-    """fetch associated terms related to the object from json"""
-    try:
-        associations = []
-        for i in _json["MensgemaaktObject.draagt"]:
-            for a in i["Werk.gaatOver"]:
-                ass = a["skos:prefLabel"]["@value"]
-                associations.append(ass)
-                df_dmg.at[range_, "association"] = associations
-            # print(associations)
-        print(associations)
-    except Exception:
-        pass
 
 for i in range(0, len(columns)):
     df_dmg.insert(i, columns[i], "")
@@ -101,6 +85,5 @@ for i in range(0, len(df_dmg)): ##change to len(df_dmg to fetch all)
     fetch_beschrijving(df_dmg, i, j)
     fetch_objectnummer(df_dmg, i, j)
     fetch_objectname(df_dmg, i, j)
-    fetch_association(df_dmg, i, j)
 
 print(df_dmg)
